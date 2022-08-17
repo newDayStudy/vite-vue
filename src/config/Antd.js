95725a94048f24ba1889d7280e23136f91a0e5f7
newDayStudy <gu4217@sina.com>
2022-08-17 09:20:33 +0800

    style: 菜单样式修改

--- src/config/Antd.js
+++ src/config/Antd.js
@@ -14,6 +14,7 @@ import {
   Modal,
   List,
   Collapse,
+  Menu,
 } from "ant-design-vue";
 function antdRegisterComponent(app) {
   app
@@ -31,6 +32,7 @@ function antdRegisterComponent(app) {
     .use(Modal)
     .use(List)
     .use(Collapse)
+    .use(Menu)
     .use(Table);
 }
 export default antdRegisterComponent;
--- src/views/HomeCom.vue
+++ src/views/HomeCom.vue
@@ -2,21 +2,21 @@
 
 <template>
   <a-layout class="a-layout">
-    <a-layout-sider theme="light">
-      <a-list bordered>
-        <a-list-item>
+    <a-layout-sider theme="dark">
+      <a-menu mode="inline" theme="dark">
+        <a-menu-item key="0">
           <router-link to="/table">表格</router-link>
-        </a-list-item>
-        <a-list-item>
+        </a-menu-item>
+        <a-menu-item key="1">
           <router-link to="/calendar"> 日历 </router-link>
-        </a-list-item>
-        <a-list-item>
+        </a-menu-item>
+        <a-menu-item key="2">
           <router-link to="/form"> 表单 </router-link>
-        </a-list-item>
-        <a-list-item>
+        </a-menu-item>
+        <a-menu-item key="3">
           <router-link to="/question"> 问卷 </router-link>
-        </a-list-item>
-      </a-list>
+        </a-menu-item>
+      </a-menu>
     </a-layout-sider>
     <a-layout-content>
       <router-view></router-view>
