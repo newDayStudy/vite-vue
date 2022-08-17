76d0fea0788db8e7c957e0c5ed9b3439eb174552
newDayStudy <gu4217@sina.com>
2022-08-17 10:10:27 +0800

    fix: 菜单当前的选中项

--- src/views/HomeCom.vue
+++ src/views/HomeCom.vue
@@ -1,19 +1,24 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const selectedKeys = ref([route.name]);
+</script>
 
 <template>
   <a-layout class="a-layout">
     <a-layout-sider theme="dark">
-      <a-menu mode="inline" theme="dark">
-        <a-menu-item key="0">
+      <a-menu v-model:selectedKeys="selectedKeys" mode="inline" theme="dark">
+        <a-menu-item key="table">
           <router-link to="/table">表格</router-link>
         </a-menu-item>
-        <a-menu-item key="1">
+        <a-menu-item key="calendar">
           <router-link to="/calendar"> 日历 </router-link>
         </a-menu-item>
-        <a-menu-item key="2">
+        <a-menu-item key="form">
           <router-link to="/form"> 表单 </router-link>
         </a-menu-item>
-        <a-menu-item key="3">
+        <a-menu-item key="question">
           <router-link to="/question"> 问卷 </router-link>
         </a-menu-item>
       </a-menu>
