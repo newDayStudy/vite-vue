45df543185c360bb2d73cd1197c12dc53a653f3f
newDayStudy <gu4217@sina.com>
2022-10-08 15:53:14 +0800

    vue3中默认插槽要以函数的形式

--- src/components/form/BaseForm.vue
+++ src/components/form/BaseForm.vue
@@ -30,47 +30,63 @@ export default {
           model: props.modelValue,
           ...props.formProps,
         },
-        [
-          h(
-            Row,
-            {
-              gutter: 20,
-            },
-            props.formItems.map((item) => {
-              const componentName = componentMap.get(item.type);
-              return h(
-                Col,
-                {
-                  span: item.col || 8,
-                },
-                h(
-                  FormItem,
-                  {
-                    key: item.formItemProps.name,
-                    ...item.formItemProps,
-                  },
-                  [
-                    item.customRender
-                      ? h(item.customRender(item))
-                      : h(componentName, {
-                          value: props.modelValue[item.formItemProps.name],
-                          onChange(e) {
-                            props.modelValue[item.formItemProps.name] =
-                              e.target.value;
-                          },
-                          placeholder: "请输入",
-                          allowClear: true,
-                          disabled: false,
-                          ...item.comProps,
-                          ...item.comOns,
-                        }),
-                  ]
-                )
-              );
-            })
-          ),
-          slots.default && slots.default(),
-        ]
+        {
+          default: () => [
+            h(
+              Row,
+              {
+                gutter: 20,
+              },
+              {
+                default: () => [
+                  props.formItems.map((item) => {
+                    const componentName = componentMap.get(item.type);
+                    return h(
+                      Col,
+                      {
+                        span: item.col || 8,
+                      },
+                      {
+                        default: () => [
+                          h(
+                            FormItem,
+                            {
+                              key: item.formItemProps.name,
+                              ...item.formItemProps,
+                            },
+                            {
+                              default: () => [
+                                item.customRender
+                                  ? h(item.customRender(item))
+                                  : h(componentName, {
+                                      value:
+                                        props.modelValue[
+                                          item.formItemProps.name
+                                        ],
+                                      onChange(e) {
+                                        props.modelValue[
+                                          item.formItemProps.name
+                                        ] = e.target.value;
+                                      },
+                                      placeholder: "请输入",
+                                      allowClear: true,
+                                      disabled: false,
+                                      ...item.comProps,
+                                      ...item.comOns,
+                                    }),
+                              ],
+                            }
+                          ),
+                        ],
+                      }
+                    );
+                  }),
+                ],
+              }
+            ),
+            slots.default && slots.default(),
+          ],
+        }
       );
   },
 };
