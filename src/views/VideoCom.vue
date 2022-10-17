7e1b091a41c932edd3ff90e57086ed458c98e204
newDayStudy <gu4217@sina.com>
2022-10-17 16:24:55 +0800

    fix:摄像头

---
+++ src/views/VideoCom.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <a-layout class="a-layout">
+    <a-card> VideoCom </a-card>
+  </a-layout>
+</template>
+
+<style lang="scss" scoped>
+.a-layout {
+  overflow-y: auto;
+  box-sizing: border-box;
+  height: 100%;
+  padding: 20px;
+}
+</style>
